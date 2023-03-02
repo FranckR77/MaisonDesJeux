9fa47c0089935718702c683a6c98a2d708366b87
Franck <franck.reignier77@gmail.com>
2023-03-02 11:31:46 +0100

    -

--- view/jeu/allJeu.html.php
+++ view/jeu/allJeu.html.php
@@ -1,10 +1,17 @@
 <?php include "$root/view/header.html.php"; ?>
+        <h1>  Jeux disponibles à la location </h1>
 
-<h1>
-    Jeux disponibles à la location
-</h1>
+        <select name="liste"">
+        <option valeur="1">Toutes Catégories</option>
+        <option valeur="2">Catégorie Carte</option>
+        <option valeur="3">Catégorie Dé</option>
+        <option valeur="4">Catégorie Plateau</option>
+        <option valeur="5">Catégorie Stratégie</option>
+        </select>
+<br>
 
 <?php
+
 foreach ($jeux as $jeu) {
     print('<img src="public/images/jeu/'.$jeu["lienimage"].'" alt="Card image cap" width="100" height="50">');
     print('<a href="/?object=jeu&action=details&id=' . $jeu["id"].'">' . $jeu["nom"].'</a><br>');
@@ -12,3 +19,20 @@ foreach ($jeux as $jeu) {
 ?>
 
 <?php include "$root/view/footer.html.php";
+
+#<form class="note" method="post" action="fiche.php">
+#<h4>Pour quel restaurant voulez-vous consulter la fiche ?</h4>
+
+#<p><select name="choix_restos" id="choix_restos" onchange="javascript:submit(this)"required >
+
+#<?php
+#$reponse = $bdd->query('SELECT * FROM restos ORDER BY nom');
+
+
+#while ($choix = $reponse->fetch())
+#{
+#?>
+
+<?php echo '<option value="'.$choix['nom'].'">'.$choix['nom'].'</option><br />';
+#}
+#?>
